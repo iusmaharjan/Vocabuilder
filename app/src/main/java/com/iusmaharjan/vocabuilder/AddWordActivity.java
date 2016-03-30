4b8aa6e0b53e14721390a772d9d4aca8aca45475
Ayush Maharjan <ius.maharjan@gmail.com>
2016-03-30 17:07:52 +0545

    Layout designed for adding words

--- app/src/main/java/com/iusmaharjan/vocabuilder/AddWordActivity.java
+++ app/src/main/java/com/iusmaharjan/vocabuilder/AddWordActivity.java
@@ -2,8 +2,13 @@ package com.iusmaharjan.vocabuilder;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.iusmaharjan.vocabuilder.main.AddWordFragment;
 
 public class AddWordActivity extends AppCompatActivity {
 
@@ -11,9 +16,20 @@ public class AddWordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_word);
+
+        if(null == savedInstanceState) {
+            initFragment(AddWordFragment.getInstance());
+        }
     }
 
     public static Intent launchAddWordActivity(Context context) {
         return new Intent(context, AddWordActivity.class);
+    }
+
+    public void initFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.contentFrame, fragment);
+        transaction.commit();
     }
 }
